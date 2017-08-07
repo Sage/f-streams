42c44c8c9cdc614d8fa736a509ad75fd48895cc2
Bruno Jouhier <bjouhier@gmail.com>
2017-08-07 21:39:55 +0200

    fixed signatures of generic reader/writer

--- lib/devices/generic.ts
+++ lib/devices/generic.ts
@@ -18,12 +18,12 @@ export const empty = {
 /// 
 /// * `reader = ez.devices.generic.reader(read[, stop])`  
 ///   creates an EZ reader from a given `read()` function and an optional `stop([arg])` function.
-export function reader<T>(read: () => T, stop?: (arg?: any) => void) {
+export function reader<T>(read: () => T | undefined, stop?: (arg?: any) => void) {
 	return new Reader(read, stop);
 }
 
 /// * `writer = ez.devices.generic.writer(write)`  
 ///   creates an ES writer from a given `write(val)` function.
-export function writer<T>(write: (value: T) => void, stop?: (arg?: any) => void) {
+export function writer<T>(write: (value: T | undefined) => void, stop?: (arg?: any) => void) {
 	return new Writer(write, stop);
 }
