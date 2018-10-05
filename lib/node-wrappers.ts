b0073b944e303e61035df5e4f999acb1d22d4267
Bruno Jouhier <bjouhier@gmail.com>
2018-10-05 12:09:54 +0200

    temp hack to fix socket.address() type definition

--- lib/node-wrappers.ts
+++ lib/node-wrappers.ts
@@ -984,7 +984,8 @@ export class SocketStream extends ReadableStream<net.Socket & NodeJS.ReadableStr
 		return this;
 	}
 	address() {
-		return this._emitter.address();
+		// TODO: remove as string cast: this is a temp hack to get around an inconsistency in node definition files
+		return this._emitter.address() as string;
 	}
 	get localAddress() { return this._emitter.localAddress; }
 	get localPort() { return this._emitter.localPort; }
